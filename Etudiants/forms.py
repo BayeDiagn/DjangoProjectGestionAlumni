9c3794543457d4ed65b24f53b12e4174161c0609
Abdoulaye Mbaye <abdoulayembaye@192.168.1.16>
2023-11-15 07:42:35 +0000

    second commit

--- Etudiants/forms.py
+++ Etudiants/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.password_validation import validate_password
-#from django.forms import ModelForm
+#from django.contrib.auth.password_validation import validate_password
 from .models import Etudiant
 
 
@@ -26,40 +25,3 @@ class EtudiantForm(UserCreationForm):
                  "adresse": forms.TextInput(attrs={'class':'form-control form-control-sm',}),
                  "tel": forms.TextInput(attrs={'class':'form-control form-control-sm',}),
                  }
-        
-        
-
-#class LoginForm(forms.Form):
- #   email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control form-control-sm','autofocus': True,'placeholder':'exemple@gmail.com'}))
-  #  password = forms.CharField(
-   #     widget=forms.PasswordInput(attrs={'class': 'form-control form-control-sm',}),
-    #    label='',)
-    
-    
-    #def clean_password(self):
-        # Récupérer la valeur du champ password
-       # password = self.cleaned_data.get('mot_de_passe')
-
-        # Valider le mot de passe en utilisant les validateurs définis dans les paramètres
-       # try:
-           # validate_password(password, user=self.instance)
-        #except forms.ValidationError as error:
-            #self.add_error('password', error)
-
-        # Retourner la valeur nettoyée
-       # return password
-    
-    
-    #def clean_password_confirm(self):
-        # Récupérer la valeur du champ password
-       # password = self.cleaned_data.get('mot_de_passe')
-
-        # Récupérer la valeur du champ password_confirm
-       # password_confirm = self.cleaned_data.get('mot_de_passe_de_confirmation')
-
-        # Valider que password et password_confirm sont égaux
-        #if password and password_confirm and password != password_confirm:
-           # raise forms.ValidationError("Les mots de passe ne correspondent pas.")
-
-        # Retourner la valeur nettoyée
-       # return password_confirm
